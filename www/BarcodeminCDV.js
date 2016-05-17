410d47946c8a61823a3a44814af2d56627d3a118
syrakozz <asapsystemsinternational@Francescas-MacBook-Air.local>
2016-05-17 15:07:02 +0300

    add android version

--- www/BarcodeminCDV.js
+++ www/BarcodeminCDV.js
@@ -10,6 +10,11 @@ module.exports = {
      * @param {Function} fail       Callback method called on failure.
      */
     scanBarcode: function(success){
+    	params =  {};
+    	params.text_title = "";
+        params.text_instructions = "";
+        params.camera = "back";
+        params.flash = "off";
         exec(success, null, 'BarcodeminCDV', 'scanBarcode', []);
     },
 
