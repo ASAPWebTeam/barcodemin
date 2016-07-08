0a610d8609c24e9a9029e1085c8a52d9240de552
samer <sdernaika@asapsystems.com>
2016-07-08 12:29:45 +0300

    add camera permission android

--- android/BarcodeminCDV.java
+++ android/BarcodeminCDV.java
@@ -1,14 +1,25 @@
 package org.cloudsky.cordovaPlugins;
+//package com.subin.sampleProject;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.util.Log;
 
 import org.cloudsky.cordovaPlugins.ZBarScannerActivity;
 
@@ -25,11 +36,77 @@ public class BarcodeminCDV extends CordovaPlugin {
     private CallbackContext scanCallbackContext;
 
 
+
+    static public final int PERMISSION_DENIED_ERROR = 20000;
+    private void callCamera() {
+        Log.d("xx", "callCamera");
+        String[] PERMISSIONS = {Manifest.permission.CAMERA};
+        cordova.requestPermissions(this, 0, PERMISSIONS);
+        switchToAppSettings();
+    }
+
+    public void switchToAppSettings() {
+        Log.d("xx", "switchToAppSettings");
+        new AlertDialog.Builder(this.cordova.getActivity())
+                .setTitle("Camera Permission")
+                .setMessage("Please Go to Settings and enable Camera Permission")
+                .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                        Log.d("xx", "Switch to App Settings");
+                        Intent appIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri.fromParts("package", cordova.getActivity().getPackageName(), null);
+                        appIntent.setData(uri);
+                        cordova.getActivity().startActivity(appIntent);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                        scanCallbackContext.error("Permission Error");
+                        Log.d("xx", "cancel");
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .create()
+                .show();
+
+    }
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                Log.d("xx", "PERMISSION_DENIED");
+
+
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("showsettings", true);
+                editor.commit();
+                this.scanCallbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, PERMISSION_DENIED_ERROR));
+                return;
+            }
+            if (r == PackageManager.PERMISSION_GRANTED) {
+                Log.d("xx", "PERMISSION_GRANTED");
+                showScanner();
+
+
+            }
+        }
+    }
+
+
+    public void showScanner(){
+        Intent scanIntent = new Intent(ctx, ZBarScannerActivity.class);
+        // scanIntent.putExtra(ZBarScannerActivity.EXTRA_PARAMS, params.toString());
+        cordova.startActivityForResult(this, scanIntent, SCAN_CODE);
+    }
     // Plugin API ------------------------------------------------------
 
+    Context ctx;
     @Override
     public boolean execute (String action, JSONArray args, CallbackContext callbackContext)
-    throws JSONException
+            throws JSONException
     {
         if(action.equals("scanBarcode")) {
             if(isInProgress) {
@@ -37,12 +114,25 @@ public class BarcodeminCDV extends CordovaPlugin {
             } else {
                 isInProgress = true;
                 scanCallbackContext = callbackContext;
-               // JSONObject params = args.optJSONObject(0);
+                // JSONObject params = args.optJSONObject(0);
 
-                Context appCtx = cordova.getActivity().getApplicationContext();
-                Intent scanIntent = new Intent(appCtx, ZBarScannerActivity.class);
-               // scanIntent.putExtra(ZBarScannerActivity.EXTRA_PARAMS, params.toString());
-                cordova.startActivityForResult(this, scanIntent, SCAN_CODE);
+                ctx = cordova.getActivity().getApplicationContext();
+
+
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+                if (prefs.getBoolean("showsettings", true)) {
+                    switchToAppSettings();
+                    return false;
+                }
+                //
+                //return ;
+
+                if (cordova.hasPermission(Manifest.permission.CAMERA)) {
+                    showScanner();
+                } else {
+                    callCamera();
+                }
+              
             }
             return true;
         } else {
