dd270cf934aadcaf3b1f50959fe44b09a4832436
ASAPWebTeam <webteam@asapsystems.com>
2016-12-22 15:59:32 +0200

    Changes on Scanning area width
    intentScan.putExtra(Intents.Scan.WIDTH, 800);

--- android/BarcodeminCDV.java
+++ android/BarcodeminCDV.java
@@ -53,7 +53,6 @@ public class BarcodeminCDV extends CordovaPlugin {
     private static final String EMAIL_TYPE = "EMAIL_TYPE";
     private static final String PHONE_TYPE = "PHONE_TYPE";
     private static final String SMS_TYPE = "SMS_TYPE";
-    public static final String WIDTH = "SCAN_WIDTH";
 
     private static final String LOG_TAG = "BarcodeminCDV";
 
@@ -183,7 +182,7 @@ public class BarcodeminCDV extends CordovaPlugin {
                            // intentScan.putExtra(Intents.Scan.ORIENTATION_LOCK, obj.optString(ORIENTATION));
                         intentScan.putExtra(Intents.Scan.ORIENTATION_LOCK, "false");
                        // }
-                        intentScan.putExtra(Intents.WIDTH, 800);
+                        intentScan.putExtra(Intents.Scan.WIDTH, 800);
                     }
 
                 }
